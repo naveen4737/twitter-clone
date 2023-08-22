98a091dfedcd6daa45694b0ed59e25879013add9
Naveen Dongre <naveendongre4737@gmail.com>
2023-08-22 21:30:47 +0530

    correcting path of client for production

--- server.js
+++ server.js
@@ -33,9 +33,9 @@ app.use("/api/v1/tweet", require("./routes/tweetRoutes"));
 
 if(process.env.NODE_ENV === "production"){
   console.log("iosdn");
-  app.use(express.static(path.join(__dirname, '../client/build')));
+  app.use(express.static(path.join(__dirname, './client/build')));
   app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "../client", "build", "index.html"))
+    res.sendFile(path.join(__dirname, "./client", "build", "index.html"))
   })
 }
 else{
